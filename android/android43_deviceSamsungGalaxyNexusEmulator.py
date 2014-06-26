6fa154aad69d4116ecf4d2218f4df3b43c131603
Albedith Diaz <albedithdiaz@gmail.com>
2014-06-26 11:50:05 -0700

    Added test example for all Android 4.3 devices

---
+++ android/android43_deviceSamsungGalaxyNexusEmulator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest 
+import os #to read enviroment variables
+from selenium import webdriver
+
+class PythonTest(unittest.TestCase):
+    def setUp(self):
+# Setting the desired_capabilities as indicated in https://saucelabs.com/platforms
+        self.desired_capabilities = webdriver.DesiredCapabilities.ANDROID
+        self.desired_capabilities['platform'] = 'Linux'
+        self.desired_capabilities['version'] = '4.3'
+        self.desired_capabilities['deviceName'] = 'Samsung Galaxy Nexus Emulator'
+        self.desired_capabilities['device-orientation'] = 'portrait'
+        self.desired_capabilities['name'] = 'Mobile Android 4.3 Test with device Samsung Galaxy Nexus Emulator'
+
+#Retreiving enviroment variables
+        SAUCE_USERNAME = os.environ.get('SAUCE_USERNAME')
+        SAUCE_ACCESS_KEY = os.environ.get('SAUCE_ACCESS_KEY')
+
+#Accessing the Sauce Labs Cloud
+        self.driver = webdriver.Remote(command_executor = ('http://' + SAUCE_USERNAME + ':' + SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub'), desired_capabilities = self.desired_capabilities)
+        self.driver.implicitly_wait(30)    
+
+    def test_sauce(self):
+        self.driver.get('http://www.google.com')
+        title = self.driver.title
+        self.assertEquals("Google", title)
+
+    def tearDown(self):
+        print("Link to your job: https://saucelabs.com/jobs/%s" % self.driver.session_id)
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+

8f32e8eb36d60cb1b55acb02a94e935661edb135
Albedith Diaz <albedith@saucelabs.com>
2016-02-29 13:00:56 -0800

    Update sample.bat

--- sample.bat
+++ sample.bat
@@ -1,4 +1,2 @@
 @echo off
-echo  74.125.239.112 www.saucelabs.com > %temp%\temphosts.txt
-type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
-copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
+echo  74.125.239.112 www.saucelabs.com >> C:\WINDOWS\system32\drivers\etc\hosts
